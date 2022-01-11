14e8a3b39b3eb7aa5207d5bfa2325abca86cc518
Amraphel <78758614+Amraphel@users.noreply.github.com>
2022-01-11 14:45:46 +0100

    Delete part6.h

--- part6.h
+++
@@ -1,9 +0,0 @@
-#include "read_relocation_table.h"
-
-int* init_tab_renum(Elf32Hdr header);
-
-Tab_Sec* renumerotation_table_section(Elf32Hdr * header, Tab_Sec* sect, int * tab_renum, int addrText, int addrData);
-
-void renumerotation_table_symbole(FILE* file,Tab_Sym* tab_sym ,Elf32Hdr header,Tab_Sec* tab_sec, int* tab_renum);
-
-void trier_table_symbole(Tab_Sym* tab_sym,Elf32Hdr header,Tab_Sec* tab_sec,FILE* file);
